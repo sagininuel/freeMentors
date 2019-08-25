3b69edf0c4016264c9641f380b2ad19bb0237500
sagininuel <sagininuel@gmail.com>
2019-08-25 14:39:01 +0300

    feature(signUp-API): fix test script on package.json file [Fixes #168074937]

--- api/src/index.js
+++ api/src/index.js
@@ -23,6 +23,6 @@ app.use('/api', apiRoutes);
 app.use('*', (req, res) => res.status(404).json({ status: 404, error: 'Page Not Found' }));
 
 app.use(errorHandler.sendError);
-app.listen(port, () => console.log(`listening on port ${port}!`));
+app.listen(port, () => console.log(`Hey Sagini, am listening on port ${port}!`));
 
 export default app;
--- api/src/models/user.js
+++ api/src/models/user.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 class user {
   /**
      * class constructor
