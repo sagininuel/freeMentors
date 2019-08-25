392a3c3f43719ad89d7c5b2940c5440ee5ac2591
sagininuel <sagininuel@gmail.com>
2019-08-25 23:11:05 +0300

    feature(view-mentors): add an API endpoint/ route to view all mentors [Start #168075588]

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,21 +1,22 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable comma-dangle */
+/* eslint-disable indent */
+/* eslint-disable linebreak-style */
 import express from 'express';
 import userRoutes from './user';
 
+const mentorRoutes = express.Router();
 
-const apiRoutes = express.Router();
+mentorRoutes.get('/', (req, res) => res.json({
+        status: 200,
+        message: 'Welcome to Free Mentors API'
+    }));
 
-apiRoutes.get('/', (req, res) => res.json({
-  status: 200,
-  message: 'Welcome to Free Mentors API',
-}));
+mentorRoutes.get('/v1', (req, res) => res.json({
+        status: 200,
+        message: 'Welcome to version 1 of Free Mentors API'
+    }));
 
-apiRoutes.get('/v1', (req, res) => res.json({
-  status: 200,
-  message: 'Welcome to version 1 of Free Mentors API',
-}));
+mentorRoutes.use('/v1/auth', userRoutes);
 
-apiRoutes.use('/v1/auth', userRoutes);
-
-
-export default apiRoutes;
+export default mentorRoutes;
