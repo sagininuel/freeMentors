21a5fcf6e5191c037c798471dac2d3a67abe8a1f
sagininuel <sagininuel@gmail.com>
2019-08-27 01:21:04 +0300

    feature(signup): add swagger documentation [Fixes #168074937]

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -1,12 +1,14 @@
 /* eslint-disable linebreak-style */
+import { config } from 'dotenv';
 import user from '../models/user';
 
+config();
 
 /**
  *
  * @class UserController
  */
-class userController {
+class UserController {
   /**
      * Create a new User
      * @static
@@ -15,7 +17,7 @@ class userController {
      * @returns { Object }
      * @memberof UserController
      */
-  static async signUp(req, res) {
+  static async signup(req, res) {
     const isTheUserRegistered = user.findOne(req.body.email);
 
     if (!isTheUserRegistered) {
@@ -24,16 +26,13 @@ class userController {
       return res.status(201).send({
         status: res.statusCode,
         data: {
-
           id: isUserRegistered.id,
           email: isUserRegistered.email,
           firstname: isUserRegistered.firstname,
           lastname: isUserRegistered.lastname,
-
         },
       });
     }
-
 
     return res.status(400).send({
       status: res.statusCode,
@@ -42,4 +41,4 @@ class userController {
   }
 }
 
-export default userController;
+export default UserController;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,20 +1,21 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable indent */
+/* eslint-disable linebreak-style */
 import express from 'express';
 import userRoutes from './user';
-
 
 const apiRoutes = express.Router();
 
 apiRoutes.get('/', (req, res) => res.json({
-  status: 200,
-  message: 'Welcome to Free Mentors API',
-}));
+        status: 200,
+        message: 'Welcome to Free Mentors API'
+    }));
 
 apiRoutes.get('/v1', (req, res) => res.json({
-  status: 200,
-  message: 'Welcome to version 1 of Free Mentors API',
-}));
+        status: 200,
+        message: 'Welcome to version 1 of Free Mentors API'
+    }));
 
 apiRoutes.use('/v1/auth', userRoutes);
-
 
 export default apiRoutes;
